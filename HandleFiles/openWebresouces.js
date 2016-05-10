ee0c380f1e2c89f221f4a4a3e82425b6f021d5f6
lior grossman <lior.grosman@gmail.com>
2016-05-10 19:52:05 +0300

    k

---
+++ HandleFiles/openWebresouces.js
@@ -0,0 +1,30 @@
+﻿, Upload: function (code, id) {
+    var webresourceurl = "/webresources/new_fileupload.htm?typename=" + code + "&id=" + id;
+
+    var DialogOptions = new Xrm.DialogOptions();
+    DialogOptions.width = 500;
+    DialogOptions.height = 300;
+
+    var crmUri = Mscrm.CrmUri.create(webresourceurl);
+    Xrm.Internal.openDialog(webresourceurl, DialogOptions, null, null,
+        function (result) {
+            alert(result);
+        }
+    );
+
+
+}
+    , ShowFolder: function (code, id) {
+        var webresourceurl = "/webresources/new_showFolder.html?typename=" + code + "&id=" + id;
+
+        var DialogOptions = new Xrm.DialogOptions();
+        DialogOptions.width = 500;
+        DialogOptions.height = 500;
+
+        var crmUri = Mscrm.CrmUri.create(webresourceurl);
+        Xrm.Internal.openDialog(webresourceurl, DialogOptions, null, null,
+            function (result) {
+                alert(result);
+            }
+        );
+    }
