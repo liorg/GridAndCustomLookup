62ca2fb2aec69ba18cba797e4dacbed46e19e34a
lior grossman <lior.grosman@gmail.com>
2016-05-19 19:37:18 +0300

    ok

---
+++ Caller/callerValidation.js
@@ -0,0 +1,17 @@
+﻿
+var Subscribes = [];
+function OnSave(ctx) {
+    var hasInValid = false;
+    for (var i = 0; i < Subscribes.length && !hasInValid; i++) {
+        var subscribe = Subscribes[i];
+        hasInValid = subscribe();
+        if (hasInValid) break;
+    }
+    if (hasInValid)
+        ctx.getEventArgs().preventDefault();
+}
+
+function AddSubscribe(subscribe) {
+    debugger;
+    Subscribes.push(subscribe);
+}
